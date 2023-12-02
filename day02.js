6bbb89a89d06b72aafd1981cc6d185a675dc986b
Manuel Sgobino <msgobino@netcologne.de>
2023-12-02 11:35:01 +0100

    day 02: sets of reveals are red herrings, only peak amounts are relevant

--- day02.js
+++ day02.js
@@ -1,31 +1,22 @@
 'use strict'
 
-const parseInput = input => input.split('\n').map(line => {
+const parseInput = input => input.split('\n')
+    .map(line => {
         const [game, cubes] = line.replace('Game ', '').split(':')
-        const reveals = cubes.split(';').map(rev => [... rev.matchAll(/(?<amount>\d+) (?<color>green|red|blue)/g)].map(e => {
+        const peaks = [... cubes.matchAll(/(?<amount>\d+) (?<color>green|red|blue)/g)].map(e => {
                 const {amount, color} = e.groups
                 return {amount: 1 * amount, color}
-            })
-        )
-        return {game: 1 * game, reveals}
+            }).reduce((acc, {amount, color}) => {
+                if (amount > acc[color]) acc[color] = amount
+                return acc}, {red: 0, green: 0, blue: 0})
+        return {game: 1 * game, peaks}
     })
 
-const maxColors = {red: 12, green: 13, blue: 14}
+const part1 = input => parseInput(input)
+    .filter(({peaks}) => peaks.red <= 12 && peaks.green <= 13 && peaks.blue <= 14)
+    .reduce((acc, {game}) => acc + game, 0)
 
-const solve = (isPart2, input) => input.reduce((result, {game, reveals}) => {
-        let valid = true
-        const peaks = {red: 0, green: 0, blue: 0}
-        for (const reveal of reveals) {
-            for (const {amount, color} of reveal) {
-                if (amount > maxColors[color]) valid = false
-                if (amount > peaks[color]) peaks[color] = amount
-            }
-        }
-        return result + ((isPart2) ? peaks.red * peaks.green * peaks.blue : valid * game)
-    }, 0)
-
-const part1 = input => solve(false, parseInput(input))
-
-const part2 = input => solve(true, parseInput(input))
+const part2 = input => parseInput(input)
+    .reduce((acc, {peaks}) => acc + peaks.red * peaks.green * peaks.blue, 0)
 
 module.exports = { part1, part2 }
