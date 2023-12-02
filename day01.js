8c88deb9fdc301d003fbd324b05fe61e67d98bda
Manuel Sgobino <msgobino@netcologne.de>
2023-12-02 12:58:49 +0100

    day 01: reduced complexity, small optimizations

--- day01.js
+++ day01.js
@@ -1,52 +1,27 @@
 'use strict'
 
-const substitutions = [
-	{ letters: "1", digit: "1" },
-	{ letters: "2", digit: "2" },
-	{ letters: "3", digit: "3" },
-	{ letters: "4", digit: "4" },
-	{ letters: "5", digit: "5" },
-	{ letters: "6", digit: "6" },
-	{ letters: "7", digit: "7" },
-	{ letters: "8", digit: "8" },
-	{ letters: "9", digit: "9" },
-	{ letters: "one", digit: "1" },
-	{ letters: "two", digit: "2" },
-	{ letters: "three", digit: "3" },
-	{ letters: "four", digit: "4" },
-	{ letters: "five", digit: "5" },
-	{ letters: "six", digit: "6" },
-	{ letters: "seven", digit: "7" },
-	{ letters: "eight", digit: "8" },
-	{ letters: "nine", digit: "9" }
-]
+const substitutions = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
 
 const findFirst = (input, subs) => {
-	let last = 1
-	while (last <= input.length) {
-		for (const n of subs) {
-			if (input.substring(0, last).includes(n.letters)) return n.digit
-		}
-		last++
-	}
+	let last = 0
+	while (last++ <= input.length)
+		for (const n of subs)
+			if (input.substring(0, last).includes(n)) return (subs.indexOf(n) % 9) + 1
 }
 
 const findLast = (input, subs) => {
 	const l = input.length
-	let first = l - 1
-	while (first >= 0) {
-		for (const n of subs) {
-			if (input.substring(first, l).includes(n.letters)) return n.digit
-		}
-		first--
-	}
+	let first = l
+	while (first-- >= 0)
+		for (const n of subs)
+			if (input.substring(first, l).includes(n)) return (subs.indexOf(n) % 9) + 1
 }
 
 const solve = (isPart2, input) => {
-    const subs = isPart2 ? substitutions : substitutions.filter(f => f.letters.length == 1)
+    const subs = isPart2 ? substitutions : substitutions.filter(f => f.length == 1)
     return input
         .split('\n')
-        .map(line => 1 * (findFirst(line, subs) + findLast(line, subs)))
+        .map(line => 10 * findFirst(line, subs) + findLast(line, subs))
         .reduce((acc, cur) => acc += cur, 0)
 }
 
