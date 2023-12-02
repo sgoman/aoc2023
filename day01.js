10aba2d10fde0578c26b9d75d73729bb1a8be714
Manuel Sgobino <msgobino@netcologne.de>
2023-12-02 15:44:18 +0100

    day 01: just using substitutions and casting strings to ints

--- day01.js
+++ day01.js
@@ -1,29 +1,25 @@
 'use strict'
 
-const substitutions = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
+const substitutions = [
+    { s: "one", r: "o1e" },
+    { s: "two", r: "t2o" },
+    { s: "three", r: "t3e" },
+    { s: "four", r: "4" },
+    { s: "five", r: "5e" },
+    { s: "six", r: "6" },
+    { s: "seven", r: "7n" },
+    { s: "eight", r: "e8t" },
+    { s: "nine", r: "n9e" }
+]
 
-const findFirst = (input, subs) => {
-	let last = 0
-	while (last++ <= input.length)
-		for (const n of subs)
-			if (input.substring(0, last).includes(n)) return (subs.indexOf(n) % 9) + 1
-}
-
-const findLast = (input, subs) => {
-	const l = input.length
-	let first = l
-	while (first-- >= 0)
-		for (const n of subs)
-			if (input.substring(first, l).includes(n)) return (subs.indexOf(n) % 9) + 1
-}
-
-const solve = (isPart2, input) => {
-    const subs = isPart2 ? substitutions : substitutions.filter(f => f.length == 1)
-    return input
-        .split('\n')
-        .map(line => 10 * findFirst(line, subs) + findLast(line, subs))
-        .reduce((acc, cur) => acc += cur, 0)
-}
+const solve = (isPart2, input) => input
+    .split('\n')
+    .map(line => {
+        if (isPart2) substitutions.forEach(sub => line = line.replaceAll(sub.s, sub.r))
+        return line.replaceAll(/\D/g, '')
+    })
+    .map(line => 1 * (line[0] + line[line.length - 1]))
+    .reduce((acc, cur) => acc += cur, 0)
 
 const part1 = input => solve(false, input)
 
