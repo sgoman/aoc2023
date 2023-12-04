d88a887122b5b0d7c2b497e931b701e80e0fddfc
Manuel Sgobino <msgobino@netcologne.de>
2023-12-04 09:18:16 +0100

    day 04: better parsing and keeping track of cards

--- day04.js
+++ day04.js
@@ -1,15 +1,13 @@
 'use strict'
 
-const parseInput = input => {
-    return input.split('\n').map(l => l.split(':')[1]).map(l => l.split('|').map(s => s.trim().replaceAll(/ +/g, ' ').split(' ')))
-}
+const parseInput = input => input.split('\n').map(l => l.split(':')[1]).map(l => l.split('|').map(s => s.trim().split(/\s+/)))
 
 const solve = (isPart2, input) => {
     const cards = Array(input.length).fill(1)
     const part1 = input.reduce((acc, [targets, attempts], index) => {
-        const correct = (attempts.filter(f => targets.includes(f))).length
+        let correct = (attempts.filter(f => targets.includes(f))).length
         if (correct) acc += Math.pow(2, correct - 1)
-        for (let i = 0; i < correct; i++) { cards[index + i + 1] += cards[index] }
+        while(correct) cards[index + correct--] += cards[index]
         return acc
     }, 0)
     return isPart2 ? cards.reduce((acc, cur) => acc + cur, 0) : part1
