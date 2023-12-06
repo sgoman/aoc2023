5b5837d915c5334a46d51944c09fd0e1c68e2ced
Manuel Sgobino <msgobino@netcologne.de>
2023-12-06 09:01:38 +0100

    day 06: comparison loop vs math - not worth it

--- day06.js
+++ day06.js
@@ -2,17 +2,22 @@
 
 const parseInput = input => {
     const nums = input.split('\n').map(l => l.split(':')[1].trim().split(/\s+/).map(Number))
-    return nums[0].map((n, i) => solve([n, nums[1][i]]))
+    return nums[0].map((n, i) => solveMath([n, nums[1][i]])).reduce((acc, cur) => acc * cur, 1)
 }
 
-const solve = ([time, dist]) => {
+const solveLoop = ([time, dist]) => {
     let wins = 0, t = 0
     while(t++ < time) if ((time - t) * t > dist) wins++
     return wins
 }
 
-const part1 = input => parseInput(input).reduce((acc, cur) => acc * cur, 1)
+const solveMath = ([time, dist]) => {
+    const root = Math.sqrt(Math.pow(time, 2) - 4 * (dist + 1))
+    return Math.floor((time + root) / 2) - Math.ceil((time - root) / 2) + 1
+}
 
-const part2 = input => solve(input.split('\n').map(l => Number(l.split(':')[1].replaceAll(/\s+/g, ''))))
+const part1 = input => parseInput(input)
+
+const part2 = input => parseInput(input.replaceAll(/ +/g, ''))
 
 module.exports = { part1, part2 }
