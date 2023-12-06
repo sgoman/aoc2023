02afbef25b9df55a466889eaf5dad3b56acf63d8
Manuel Sgobino <msgobino@netcologne.de>
2023-12-06 08:05:47 +0100

    day 06: removed unnecessary code, simple solve function

--- day06.js
+++ day06.js
@@ -2,35 +2,17 @@
 
 const parseInput = input => {
     const nums = input.split('\n').map(l => l.split(':')[1].trim().split(/\s+/).map(Number))
-    return nums[0].map((n, i) => [n, nums[1][i]])
+    return nums[0].map((n, i) => solve([n, nums[1][i]]))
 }
 
-const solve = (isPart2, input) => {
-    const races = input.reduce((acc, [time, dist]) => {
-        let t = 0
-        const wins = []
-        while(t++ < time) {
-            const d = (time - t) * t
-            if (d > dist) wins.push({t, d})
-        }
-        acc.push(wins)
-        return acc
-    }, [])
-    if (!isPart2) {
-        return races.reduce((acc, cur) => acc * cur.length, 1)
-    }
-}
-
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
-
-const part2 = input => {
-    const [time, dist] = input.split('\n').map(l => Number(l.split(':')[1].replaceAll(/\s+/g, '')))
-    let wins = 0
-    let t = 0
+const solve = ([time, dist]) => {
+    let wins = 0, t = 0
     while(t++ < time) if ((time - t) * t > dist) wins++
     return wins
 }
+
+const part1 = input => parseInput(input).reduce((acc, cur) => acc * cur, 1)
+
+const part2 = input => solve(input.split('\n').map(l => Number(l.split(':')[1].replaceAll(/\s+/g, ''))))
 
 module.exports = { part1, part2 }
