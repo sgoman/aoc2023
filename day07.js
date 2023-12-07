d8c09afdd095fadd8838096c1f681b2d5ec77a0a
Manuel Sgobino <msgobino@netcologne.de>
2023-12-07 17:22:14 +0100

    day 07: smarter array usage and better names

--- day07.js
+++ day07.js
@@ -1,67 +1,35 @@
 'use strict'
 
-const translations = [
-	{ "from": "T", "to": "B" },
-	{ "from": "J", "to": "C" },
-	{ "from": "Q", "to": "D" },
-	{ "from": "K", "to": "E" },
-	{ "from": "A", "to": "F" }
-]
+const translations = [ ["T", "B"], ["J", "C"], ["Q", "D"], ["K", "E"], ["A", "F"] ] // for easy sorting
 
-const kind = hand => {
-	let k = {}
-	hand.split('').forEach(c => {
-		if (Object.keys(k).includes(c)) {
-			k[c] = k[c] + 1
+const type = hand => {
+	let freq = {}
+	hand.split('').forEach(c => { freq[c] = (Object.keys(freq).includes(c)) ? freq[c] + 1 : 1 })
+	if (Object.keys(freq).includes('1')) { // this block handles jokers of part 2
+		const joker = freq['1']
+		freq = Object.fromEntries(Object.entries(freq).filter(f => f[0] != '1'))
+		if (Object.values(freq).length) {
+			const best = Object.entries(freq).sort((a, b) => b[1] - a[1] || b[0].localeCompare(a[0]))[0][0]
+			freq[best] = freq[best] + joker
 		} else {
-			k[c] = 1
-		}
-	})
-	if (Object.keys(k).includes('1')) {
-		const joker = k
-		k = Object.fromEntries(Object.entries(k).filter(f => f[0] != '1'))
-		if (Object.values(k).length) {
-			const best = Object.entries(k).sort((a, b) => b[1] - a[1] || b[0].localeCompare(a[0]))[0][0]
-			k[best] = k[best] + joker[1]
-		} else {
-			k[joker[0]] = joker[1]
+			freq['1'] = joker
 		}
 	}
-	const score = Object.values(k).sort((a, b) => b - a).join('')
-	if (score == '5') {
-		return 7
-	} else if (score == "41") {
-		return 6
-	} else if (score == "32") {
-		return 5
-	} else if (score == "311") {
-		return 4
-	} else if (score == "221") {
-		return 3
-	} else if (score == "2111") {
-		return 2
-	} else if (score == "11111") {
-		return 1
-	} else {
-		return 0
-	}
+	const score = Object.values(freq).sort((a, b) => b - a).join('') // sorted frequencies, descending order
+    return ["0", "11111", "2111", "221", "311", "32", "41", "5"].indexOf(score)
 }
 
-const parseInput = input => {
-    return input.split('\n').map(l => {
+const solve = input => input.split('\n')
+    .map(l => { // parsing the input
 	    let [hand, bid] = l.split(' ')
-	    translations.forEach(t => { while(hand.includes(t.from)) hand = hand.replace(t.from, t.to)} )
-	    return {hand, bid: Number(bid), kind: kind(hand)}
-    }).sort((a, b) => a.kind - b.kind || a.hand.localeCompare(b.hand))
-}
+	    translations.forEach(([from, to]) => { hand = hand.replaceAll(from, to) })
+	    return {hand, bid: Number(bid), type: type(hand)}
+    })
+    .sort((a, b) => a.type - b.type || a.hand.localeCompare(b.hand)) // rank the hands
+    .reduce((acc, cur, i) => acc + (i + 1) * cur.bid, 0) // tally up
 
-const solve = input => input.reduce((acc, cur, i) => acc + (i + 1) * cur.bid, 0)
+const part1 = input => solve(input)
 
-const part1 = input => solve(parseInput(input))
-
-const part2 = input => {
-	while(input.includes('J')) input = input.replace('J', '1')
-    return solve(parseInput(input))
-}
+const part2 = input => solve(input.replaceAll('J', '1')) // another easy sorting hack...
 
 module.exports = { part1, part2 }
