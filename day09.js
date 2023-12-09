b58214ace54a94f51a1d9a5229b280dfdc035823
Manuel Sgobino <msgobino@netcologne.de>
2023-12-09 07:39:04 +0100

    day 09: all loops done with map and reduce

--- day09.js
+++ day09.js
@@ -2,15 +2,14 @@
 
 const parseInput = input => input.split('\n').map(l => l.split(' ').map(Number))
 
+const diffs = sequence => sequence.reduce((seq, cur, i, arr) => { if (i) seq.push(cur - arr[i - 1]); return seq }, [])
+
 const solveSequence = (seq, isPart2) => {
     if (seq.every(e => e == 0)) return 0
-
-    const s = [], l = seq.length - 1
-    for (let i = 0; i < l; i++) s.push(seq[i + 1] - seq[i])
-    return isPart2 ? seq[0] - solveSequence(s, isPart2) : seq[l] + solveSequence(s, isPart2)
+    return isPart2 ? seq[0] - solveSequence(diffs(seq), isPart2) : seq[seq.length - 1] + solveSequence(diffs(seq), isPart2)
 }
 
-const solve = (isPart2, input) => input.reduce((total, seq) => total +  solveSequence(seq, isPart2), 0)
+const solve = (isPart2, input) => input.reduce((total, seq) => total + solveSequence(seq, isPart2), 0)
 
 const part1 = input => solve(false, parseInput(input))
 
