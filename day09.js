e6d8bb95ac05f7852db5b6d8935f2b0d1c3a6e11
Manuel Sgobino <msgobino@netcologne.de>
2023-12-09 07:15:50 +0100

    day 09: D'oh! Why did I keep the unnecessary prev array?

--- day09.js
+++ day09.js
@@ -2,15 +2,15 @@
 
 const parseInput = input => input.split('\n').map(l => l.split(' ').map(Number))
 
-const solveSequence = (seq, prev, isPart2) => {
+const solveSequence = (seq, isPart2) => {
     if (seq.every(e => e == 0)) return 0
 
     const s = [], l = seq.length - 1
     for (let i = 0; i < l; i++) s.push(seq[i + 1] - seq[i])
-    return isPart2 ? seq[0] - solveSequence(s, seq, isPart2) : seq[l] + solveSequence(s, seq, isPart2)
+    return isPart2 ? seq[0] - solveSequence(s, isPart2) : seq[l] + solveSequence(s, isPart2)
 }
 
-const solve = (isPart2, input) => input.reduce((total, seq) => total +  solveSequence(seq, [0], isPart2), 0)
+const solve = (isPart2, input) => input.reduce((total, seq) => total +  solveSequence(seq, isPart2), 0)
 
 const part1 = input => solve(false, parseInput(input))
 
