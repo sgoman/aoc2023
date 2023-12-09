7027192b710d276eb8585681966900236401bc7b
Manuel Sgobino <msgobino@netcologne.de>
2023-12-09 06:44:41 +0100

    day 09: challenge commit for part 2, already optimized?

--- day09.js
+++ day09.js
@@ -1,23 +1,19 @@
 'use strict'
 
-const parseInput = input => {
-    return input.split('\n').map(l => l.split(' ').map(Number))
-}
+const parseInput = input => input.split('\n').map(l => l.split(' ').map(Number))
 
-const solveSequence = (seq, prev) => {
+const solveSequence = (seq, prev, isPart2) => {
     if (seq.every(e => e == 0)) return 0
 
     const s = [], l = seq.length - 1
     for (let i = 0; i < l; i++) s.push(seq[i + 1] - seq[i])
-    return seq[l] + solveSequence(s, seq)
+    return isPart2 ? seq[0] - solveSequence(s, seq, isPart2) : seq[l] + solveSequence(s, seq, isPart2)
 }
 
-const solve = (isPart2, input) => input.reduce((total, seq) => total +  solveSequence(seq, [0]), 0)
+const solve = (isPart2, input) => input.reduce((total, seq) => total +  solveSequence(seq, [0], isPart2), 0)
 
 const part1 = input => solve(false, parseInput(input))
 
-const part2 = input => {
-    return solve(true, parseInput(input))
-}
+const part2 = input => solve(true, parseInput(input))
 
 module.exports = { part1, part2 }
