8480a4b0e3425fa2c939f161fb4eb4d7eea6b792
Manuel Sgobino <msgobino@netcologne.de>
2023-12-11 09:31:23 +0100

    day 11: challenge commit for part 2

--- day11.js
+++ day11.js
@@ -1,19 +1,24 @@
 'use strict'
 
-const parseInput = input => {
+const parseInput = (input, expansion) => {
     let grid = input.split('\n').map(l => l.split(''))
     const emptyRows = grid.map((l, i) => [l, i]).filter(e => e[0].every(c => c == '.')).map(e => e[1])
     let emptyCols = []
     for (let c = 0; c < grid[0].length; c++) {
         if (grid.map(l => l[c]).every(e => e == '.')) emptyCols.push(c)
     }
-    emptyRows.reverse().forEach(row => grid.splice(row, 0, grid[row]))
-    // I'm ignoring the bug that adds extra empty space on empty lines from the next instruction...
-    emptyCols.reverse().forEach(col => grid = grid.reduce((acc, row) => { row.splice(col, 0, '.'); acc.push(row); return acc}, []))
     const galaxies = []
     for (let r = 0; r < grid.length; r++)
         for (let c = 0; c < grid[0].length; c++)
-            if (grid[r][c] == '#') galaxies.push({id: galaxies.length, row: r, col: c})
+            if (grid[r][c] == '#') {
+                const expandedRows = emptyRows.filter(f => f < r).length
+                const expandedCols = emptyCols.filter(f => f < c).length
+                galaxies.push({
+                    id: galaxies.length,
+                    row: expandedRows * expansion - expandedRows + r,
+                    col: expandedCols * expansion - expandedCols + c
+                })
+            }
     return galaxies
 }
 
@@ -21,16 +26,10 @@ const pairs = (arr) => arr.map( (v, i) => arr.slice(i + 1).map(w => [v, w]) ).fl
 
 const manhattan = (a, b) => Math.abs(b.row - a.row) + Math.abs(b.col - a.col)
 
-const solve = (isPart2, input) => {
-    return pairs(input).reduce((acc, [a, b]) => acc + manhattan(a, b), 0)
-}
+const solve = input => pairs(input).reduce((acc, [a, b]) => acc + manhattan(a, b), 0)
 
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
+const part1 = input => solve(parseInput(input, 2))
 
-const part2 = input => {
-    return solve(true, parseInput(input))
-}
+const part2 = input => solve(parseInput(input, 1e6))
 
 module.exports = { part1, part2 }
