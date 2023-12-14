6b55c7472e5cb47bc8a53a834055bd6a61f1bc9c
Manuel Sgobino <msgobino@netcologne.de>
2023-12-14 13:20:16 +0100

    day 14: challenge commit for part 2

--- day14.js
+++ day14.js
@@ -7,45 +7,35 @@ const transpose = block => block.reduce((cols, row, r, arr) => {
     return cols
 }, [])
 
-const tiltNorth = grid => {
-    for (let r = grid.length - 1, h = grid.length; r > 0; r--) {
-        for (let c = 0, w = grid[0].length; c < w; c++) {
-            let a = r - 1
-            while (a >= 0 && grid[a][c] != '#') a--
-            a = Math.max(0, a)
-            while (a < r && grid[a][c] != '.') a++
-            a = Math.min(a, r - 1)
-            if (grid[r][c] == 'O' && grid[a][c] == '.') {
-                grid[r][c] = '.'
-                grid[a][c] = 'O'
-            }
-        }
-    }
-    return grid
-}
+const tiltNorth = grid => grid.map(line => line.join('').split('#').map(s => s.split('').sort().join('')).join('#').split(''))
 
-const load = input =>input.reduce((acc, line, row, arr) => acc + (line.filter(f => f == 'O').length * (arr.length - row)), 0)
+const load = input =>input.reduce((acc, line, row, arr) => acc + line.reduce((a, c, i) => a + (c == 'O' ? i + 1 : 0), 0), 0)
 
-const part1 = input => load(tiltNorth(parseInput(input)))
+const key = input => input.map(l => l.join('')).join('')
+
+const part1 = input => load(tiltNorth(transpose(parseInput(input))))
 
 const part2 = input => {
     input = parseInput(input)
-    let cycle = 0, prev = -1, l = 0
-    while (cycle <= 4e9) {
-        input = tiltNorth(input)
-        l = load(input)
-        if (cycle % 4 == 0) {
+    const seen = {}, loop = 4e9
+    for (let cycle = 0; cycle <= loop; cycle++) {
+        input = tiltNorth(transpose(input))
+        let l = 0
+        switch(cycle % 4) {
+            case 3: l = load(transpose(transpose(transpose(input)))); break
+            case 2: l = load(transpose(transpose(input))); break
+            case 1: l = load(transpose(input)); break
+            case 0: l = load(input); break
         }
-        input = transpose(input)
-        if (cycle % 4 == 3) {
-            if (l == prev) return l
-            prev = l
-            // for(const line of input) console.log(line.join(''))
-            // console.log()
+        const k = key(input)
+        if (Object.hasOwn(seen, k)) {
+            const s = seen[k][0]
+            const m = (loop - cycle - 2) % (cycle - seen[k][0])
+            const [[i, v]] = Object.values(seen).filter(f => f[0] == s + m)
+            return v
         }
-        cycle++
+        seen[k] = [cycle, l]
     }
-    return l
 }
 
 module.exports = { part1, part2 }
