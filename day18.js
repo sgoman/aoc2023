8c38cad9f385c3befcbdbf350d80103efffd69d7
Manuel Sgobino <msgobino@netcologne.de>
2023-12-18 11:05:27 +0100

    day 18: challenge commit for part 2, quite optimized

--- day18.js
+++ day18.js
@@ -1,60 +1,31 @@
 'use strict'
 
-const parseInput = input => input.split('\n').map(l => l.split(' ')).map(([command, dist, color]) => ["URDL".indexOf(command), Number(dist), color.replace('(', '').replace(')', '')])
+const parseInput = input => input.split('\n').map(l => l.split(' '))
+	.map(([command, dist, color]) => [
+		"URDL".indexOf(command),
+		Number(dist),
+		(Number(color[7]) + 1) % 4,
+		parseInt(color.slice(2,-2), 16)
+	])
 
 const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-const walk = parsed => {
-	const tiles = []
-	let row = 0, col = 0
-	for (const [command, dist, color] of parsed) {
-		for (const _ of Array(dist)) {
-			row += dirs[command][0]
-			col += dirs[command][1]
-			tiles.push([row, col])
-		}
-	}
-	return tiles
+const solve = parsed => {
+	const [area, boundary] = parsed.reduce(([a, d, row, col, prevR, prevC], [command, dist]) => {
+		const [dr, dc] = dirs[command]
+		prevR = row
+		prevC = col
+		row += dr * dist
+		col += dc * dist
+		a += prevC * row - col * prevR
+		d += dist
+		return [a, d, row, col, prevR, prevC]
+	}, [0, 0, 0, 0, 0, 0])
+	return Math.abs(area / 2) + boundary / 2 + 1
 }
 
-const solve = (isPart2, input) => {
-	const walked = walk(input)
-	const minRow = Math.min(...walked.map(e => e[0]))
-	const maxRow = Math.max(...walked.map(e => e[0]))
-	const minCol = Math.min(...walked.map(e => e[1]))
-	const maxCol = Math.max(...walked.map(e => e[1]))
-	const rowOffset = 0 - minRow, colOffset = 0 - minCol
-	const grid = walked.reduce((acc, [row, col]) => {
-		acc[row + rowOffset][col + colOffset] = '#'
-		return acc
-	}, Array(maxRow - minRow + 1).fill(0).map(_ => Array(maxCol - minCol + 1).fill(0).map(() => '.')))
-	const colStart = grid[1].join('').indexOf('#.') + 1
-	// console.log({minRow, maxRow, minCol, maxCol, rowOffset, colOffset, colStart})
-	const visited = new Set()
-	const bfs = [[1, colStart]]
-	while (bfs.length) {
-		const [row, col] = bfs.shift()
-		grid[row][col] = '#'
-		for (const [dr, dc] of dirs) {
-			const nr = row + dr, nc = col + dc
-			const k = '' + nr + ';' + nc
-			if (grid[nr][nc] == '.' && !visited.has(k)) {
-				bfs.push([nr, nc])
-				visited.add(k)
-			}
-		}
-	}
-	// console.log(grid.slice(0, 16).map(l => l.slice(0, 170).join('')).join('\n'))
-	// console.log(grid.map(l => l.join('')).join('\n'))
-    return grid.reduce((acc, line) => acc + line.filter(t => t == '#').length, 0)
-}
+const part1 = input => solve(parseInput(input))
 
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
-
-const part2 = input => {
-    return solve(true, parseInput(input))
-}
+const part2 = input => solve(parseInput(input).map(l => l.slice(2)))
 
 module.exports = { part1, part2 }
