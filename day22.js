318eeab934fbdfb19fb27ef56afd0c8c49f9d1e8
Manuel Sgobino <msgobino@netcologne.de>
2023-12-22 16:11:08 +0100

    day 22: challenge commit for part 2

--- day22.js
+++ day22.js
@@ -1,28 +1,19 @@
 'use strict'
 
-const parseInput = input => input.split('\n').map(l => l.split(/[,~]/).map(Number))
+const parseInput = input => input.split('\n').map(l => l.split(/[,~]/).map(Number)).sort((a, b) => a[2] - b[2])
 
 const hash = (x, y, z) => [x, y, z].join(',')
 
-const isSupported = (cubeSet, x, y, z) => {
-    if (z == 0) return true
-    return cubeSet.has(hash(x, y, z))
-}
+const isSupported = (cubeSet, x, y, z) => (z == 0) ? true : cubeSet.has(hash(x, y, z))
 
 const drop = bricks => {
-    let dropped = false
+    let dropped = 0
     const cubeSet = new Set()
-    bricks = bricks.sort((a, b) => a[2] - b[2])
-    for (const [sx, sy, sz, ex, ey, ez] of bricks) {
-        for (let x = sx; x < ex + 1; x++) {
-            for (let y = sy; y < ey + 1; y++) {
-                for (let z = sz; z < ez + 1; z++) {
+    for (const [sx, sy, sz, ex, ey, ez] of bricks)
+        for (let x = sx; x < ex + 1; x++)
+            for (let y = sy; y < ey + 1; y++)
+                for (let z = sz; z < ez + 1; z++)
                     cubeSet.add(hash(x, y, z))
-                }
-            }
-        }
-    }
-    // console.log({cubeSet})
     const b = []
     for (const [sx, sy, sz, ex, ey, ez] of bricks) {
         let supported = false
@@ -38,33 +29,31 @@ const drop = bricks => {
         if (supported) {
             b.push([sx, sy, sz, ex, ey, ez])
         } else {
-            dropped = true
+            dropped++
             b.push([sx, sy, sz - 1, ex, ey, ez - 1])
+            for (let x = sx; x < ex + 1; x++) {
+                for (let y = sy; y < ey +1; y++) {
+                    cubeSet.delete(hash(x, y, ez))
+                    cubeSet.add(hash(x, y, sz - 1))
+                }
+            }
         }
     }
     return [b, dropped]
 }
 
 const solve = (isPart2, bricks) => {
-    let result = 0, dropped = true, d = 0
-    // console.log({bricks})
-    while (dropped) {
-        [bricks, dropped] = drop(bricks)
-        d++
-        // console.log({b: bricks.length, d, dropped, bricks})
-    }
+    let result = 0, dropped = 1, d = 0
+    while (dropped) [bricks, dropped] = drop(bricks)
     for (let i = 0; i < bricks.length; i++) {
-        result += drop(bricks.filter((f, j) => j != i))[1] ? 0 : 1
+        [d, dropped] = drop(bricks.filter((f, j) => j != i))
+        result += isPart2 ? dropped : !dropped
     }
     return result
 }
 
-const part1 = input => {
-    return solve(false, parseInput(input))
-}
+const part1 = input => solve(false, parseInput(input))
 
-const part2 = input => {
-    return solve(true, parseInput(input))
-}
+const part2 = input => solve(true, parseInput(input))
 
 module.exports = { part1, part2 }
